f8bedb4a03f51a09765a5403898d342d8deb27ab
omkar <omkar@xtra.co.nz>
2020-03-04 12:30:31 +1300

    modified cells, added sku column, added total

--- models/invoice_model.py
+++ models/invoice_model.py
@@ -9,11 +9,11 @@ class Invoice:
         self.pdf.set_title('invoice')
         self.pdf.set_font('Arial', 'B', 8)
     
-    def write_invoice_content(self):
+    def write_invoice_content(self, total):
         self.pdf.add_page()
 
         self.write_header()
-        self.write_content()
+        self.write_content(total)
 
     def write_header(self):
         #image and title
@@ -34,39 +34,50 @@ class Invoice:
         self.write_text(35, 54, f'From: {self.data["company_name"]}')
         self.write_text(35, 60, f'To: {self.data["invoice_to"]}')
     
-    def write_content(self):
+    def write_content(self, total):
         #draw cells
+        self.pdf.set_line_width(0.2)
         self.pdf.set_xy(15, 70)
-        self.pdf.cell(180, 200, '', border=1)
+        self.pdf.cell(180, 190, '', border=1)
         self.pdf.set_xy(15, 70)
-        self.pdf.cell(125, 200, '', border=1)
+        self.pdf.cell(125, 190, '', border=1)
+        self.pdf.set_xy(110, 70)
+        self.pdf.cell(30, 190, '', border=1)
         self.pdf.set_xy(140, 70)
-        self.pdf.cell(30, 200, '', border=1)
+        self.pdf.cell(30, 190, '', border=1)
+        self.pdf.set_xy(140, 260)
+        self.pdf.cell(55, 15, '', border=1)
         
         #write titles and underline (line width 0.5mm, then reset to default)
         self.pdf.set_font('Arial', 'B', 12)
         self.write_text(18, 75, 'Product name:')
+        self.write_text(112, 75, 'SKU:')
         self.write_text(142, 75, 'Quantity:')
         self.write_text(172, 75, 'Price:')
-        self.pdf.set_line_width(0.5)
-        self.pdf.line(18, 76, 185, 76)
+        self.pdf.set_line_width(0.3)
+        self.pdf.line(15, 76.5, 195, 76.5)
         self.pdf.set_line_width(0.2)
 
         self.pdf.set_font('Arial', size=10)
         products = self.data["products"]
-        y = 80
+        y = 81
 
         for _ in range(7):
             for product in products:
                 title = products[product]["title"]
                 quantity = products[product]["quantity"]
                 price = products[product]["price"]
+                sku = products[product]["sku"]
 
                 self.write_text(18, y, title)
+                self.write_text(112, y, str(sku))
                 self.write_text(142, y, str(quantity))
                 self.write_text(172, y, str(price))
                         
                 y += 5
+
+        self.pdf.set_font('Arial', 'B', 22)
+        self.write_text(145, 270, '$' + str(total) + ' due')
         
     def write_text(self, x, y, text):
         self.pdf.text(x, y, text)
