0ea7a25bd8032b276b4d1bb9bf52e2af78fcf58c
omkar <omkar@xtra.co.nz>
2020-03-04 12:28:19 +1300

    added total calculation logic and passed total to invoice object to display

--- services/invoice_service.py
+++ services/invoice_service.py
@@ -3,6 +3,23 @@ from models.invoice_model import Invoice
 def generate_invoice(data):
     invoice = Invoice(data)
 
-    invoice.write_invoice_content()
+    total = get_total(data)
+
+    invoice.write_invoice_content(total)
 
     invoice.output('invoice.pdf')
+
+def get_total(data):
+    total = 0
+
+    products = data["products"]
+
+    for product in products:
+        quantity = products[product]["quantity"]
+        price = products[product]["price"]
+
+        value = quantity * price
+
+        total += value
+    
+    return total
